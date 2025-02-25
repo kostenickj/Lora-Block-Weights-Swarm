b0f2075c28d1f311a1f6f9b2a682f6fb6bc111a6
Jacob Kostenick <kostenickj@gmail.com>
2025-02-25 01:23:51 -0800

    fix install button

--- assets/lora-block-weight.js
+++ assets/lora-block-weight.js
@@ -1,5 +1,5 @@
 postParamBuildSteps.push(() => {
-    let group = document.getElementById('input_group_content_lora_block_weights_loader');
+    let group = document.getElementById('input_group_loraloaderblockweight');
     if (group) {
         if (!currentBackendFeatureSet.includes('lora_block_weights_loader')) {
             group.append(createDiv(`lora_block_weights_loader_install_button`, 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('lora_block_weights_loader', 'lora_block_weights_loader_install_button')">Install</button>`));
